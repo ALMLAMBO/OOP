fcd9562c7d0c928b0171c7566e60ef9e70c4211e
ALMLAMBO <aleksm007148@gmail.com>
2018-12-30 12:28:22 +0200

    class notes

---
+++ C++/classwork/2018-09-18-intro/input.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int val;
+	while(true) {
+		cout << "val = ";
+		cin >> val;
+		if(!cin) {
+			break;
+		}
+		cout << "val = " << val << endl;
+	}
+	return 0;
+}
---
+++ C++/classwork/2018-09-18-intro/input1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int val;
+	while(true) {
+		cin >> val;
+		if(!cin) {
+			break;
+		}
+		cout << val << endl;
+	}
+	return 0;
+}
