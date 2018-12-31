3371f84eee34fe0a01f781711bcb6519a7d9a04c
ALMLAMBO <aleksm007148@gmail.com>
2018-12-31 15:39:29 +0200

    postfix thrid part

---
+++ C++/classwork/2018-12-11-postfix3/negate.hh
@@ -0,0 +1,17 @@
+#ifndef NEGATE_HH__
+#define NEGATE_HH_
+
+#include "unary.hh"
+
+class Negate : public UnaryOperation{
+public:
+
+	Negate(Calculator& calc)
+	: UnaryOperation("neg", calc) {}
+
+protected:
+
+	double eval(double value);
+};
+
+#endif
