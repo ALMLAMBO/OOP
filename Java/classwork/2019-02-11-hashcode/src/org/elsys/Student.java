7272938bb4b660960dd774e1b7e30677d0190c98
ALMLAMBO <aleksm007148@gmail.com>
2019-02-23 11:22:31 +0200

    Class notes on hash code, overriding methods and introduction to junit tests

--- Java/classwork/2019-02-11-hashcode/src/org/elsys/Student.java
+++ Java/classwork/2019-02-11-hashcode/src/org/elsys/Student.java
@@ -1,53 +1,91 @@
 package org.elsys;
 
+import java.util.LinkedList;
+
 public class Student {
     private long id;
     private String FirstName;
     private String LastName;
-    public Student() {
+    private LinkedList<Double> grades;
+
+    public Student(){
         this.id = 0;
         this.FirstName = this.LastName = "";
+        this.grades = new LinkedList<Double>();
     }
 
-    public Student(long id, String FirstName, String LastName) {
+    public Student(long id, String fName, String lName){
         this.id = id;
-        this.FirstName = FirstName;
-        this.LastName = LastName;
+        this.FirstName = fName;
+        this.LastName = lName;
+        this.grades = new LinkedList<Double>();
     }
 
-    public long getId() {
-        return id;
+    public long getId(){
+        return this.id;
     }
 
-    public String getFirstName() {
-        return FirstName;
+    public String getFirstName(){
+        return this.FirstName;
     }
 
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.FirstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.LastName = lastName;
+    public String getLastName(){
+        return this.LastName;
     }
 
     @Override
-    public String toString() {
-        return id
-                + " " + FirstName
-                + " " + LastName;
+    public String toString(){
+        return "Student: "
+                + id + " "
+                + FirstName + " "
+                + LastName;
     }
 
-    /*@Override
-    public boolean equals(Student other) {
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null) {
+            if (this != null) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else if (this == null) {
+            return false;
+        }
+        Student other = (Student)obj;
+        if (id != other.id) {
+            return false;
+        }
+        if (!FirstName.equals(other.FirstName)) {
+            return false;
+        }
+        if (!LastName.equals(other.LastName)) {
+            return false;
+        }
+        return true;
+    }
 
-    }*/
+    @Override
+    public int hashCode(){
+        int result = 0;
+        final int prime = 31;
+        result = FirstName.hashCode() * prime
+                + LastName.hashCode()
+                + (int)id;
+        return result;
+    }
+
+    public void addGrade(Double grade){
+        this.grades.add(grade);
+    }
+
+    public Double avarageGrade(){
+        Double result = 0d;
+        for (Double grade : grades) {
+            result += grade;
+        }
+        return result / grades.size();
+    }
 }
